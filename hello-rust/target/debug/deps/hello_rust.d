b60ca5b26e5a2f9d1d3ec170f750989ea416db95
Aphiwatch <Aphiwatch09@gmail.com>
2024-12-12 11:49:40 +0700

    add

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\Users\aphiw\Documents\Learn-Rust\hello-rust\target\debug\deps\hello_rust.exe: src/main.rs
+
+C:\Users\aphiw\Documents\Learn-Rust\hello-rust\target\debug\deps\hello_rust.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+C:\Users\aphiw\Documents\Learn-Rust\hello-rust\target\debug\hello-rust.exe: C:\Users\aphiw\Documents\Learn-Rust\hello-rust\src\main.rs
