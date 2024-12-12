5d41de5c300721ad1003b93ab4f0464addceb4af
Aphiwatch <Aphiwatch09@gmail.com>
2024-12-12 11:56:36 +0700

    delete hello-rust project

--- hello-rust/target/debug/deps/hello_rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aphiw\Documents\Learn-Rust\hello-rust\target\debug\deps\hello_rust.exe: src/main.rs
-
-C:\Users\aphiw\Documents\Learn-Rust\hello-rust\target\debug\deps\hello_rust.d: src/main.rs
-
-src/main.rs:
